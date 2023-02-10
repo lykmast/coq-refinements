258a4c9fa15d3d30767e473b1b40a9d9e2530a73
lykmast <lykmast@gmail.com>
2023-02-10 19:08:08 +0100

    apply some of the previous tactics to Ack

--- theories/Ack.v
+++ theories/Ack.v
@@ -86,18 +86,17 @@ simp ack. case_eq (`m) 0; destruct matches; try my_trivial.
   - my_trivial.
   - rewrite Heqrhs. my_trivial.
 Qed.
-
 Theorem ack_mon_snd (m n: Nat) (p: {v:Z| `n > v /\ v >= 0}): ` (ack m n) > ` (ack m (ltac:(infer p))).
 Proof.
   revert m p. induction n as [n ack_mon_snd] using nat_lt_ind. intros m p.
   case_eq (`n) (`p+1);  try my_trivial.
-  + applys_eq (ack_snd_plus_one m (ltac:(upcast p))); try unfold addn1; my_trivial.
-  + eapply gt_gt_gt.
-Require Import ProofIrrelevance.
-    * applys_eq (ack_snd_plus_one m (ltac:(infer (``(`n - 1))))).
-      destructRef n; destructRef m. repeat f_equal. apply subset_eq_compat. simpl in *. lia.
-    * applys_eq (ack_mon_snd ltac:(infer (``(`n-1))) m (ltac:(refine (exist _ (` p) _); destructRef p; smt_infer)))
-      ; destructRef m; destructRef n; destructRef p; repeat f_equal; apply subset_eq_compat; simpl in *. all:lia.
+  + reft_apply ack_snd_plus_one m p ; try unfold addn1; my_trivial.
+  + set (nm1:= (``(`n-1))). reft_pose ack_snd_plus_one m  nm1.
+    reft_pose ack_mon_snd nm1 m p.
+    eapply gt_gt_gt.
+    * applys_eq H1. resolve_eq.
+
+    * applys_eq H5; resolve_eq.
 Qed.
 
 
@@ -105,7 +104,7 @@ Theorem ack_mon_eq_snd (m:Nat) (n: Nat) (p:{v:Z | `n >= v /\ v >= 0}): ` (ack m 
 Proof.
   case_eq (`n) (`p); try my_trivial.
   + apply eq_ge. my_trivial.
-  + apply gt_ge2. applys_eq (ack_mon_snd m n ltac:(infer p)). my_trivial.
+  + apply gt_ge2. reft_apply ack_mon_snd m n p; resolve_eq.
 Qed.
 
 Check Nat.
@@ -128,32 +127,27 @@ Theorem reorder_forall {A} {B} {P:A -> B -> Prop}: (forall (x:A) (y:B) , P x y) 
 intro. intros. apply H. Qed.
 
 
-(* Lemma gt_ *)
-
 Theorem ack_plus_one_fst_snd m n : ` (ack (addn1 m) n) >= ` (ack m (addn1 n)).
   revert m. induction n as [n IHn] using nat_lt_ind.
   pose (reorder_forall IHn) as ack_plus_one_fst_snd.
   intro m. remember (ack m (addn1 n)) as rhs. simp ack; case_eq (`n) 0; destruct matches. all:try my_trivial.
   + apply eq_ge. rewrite Heqrhs. unfold addn1. my_trivial.
   + rewrite Heqrhs.
-  assert (`n - 1 >= 0 /\ `n-1 < `n) by (destructAllRefts; smt_infer).
-  set (nm1 := ltac:(exists (`n-1); assumption):{v:Z| v>=0 /\ v < (`n)} ).
-  (* pose proof (ack_plus_one_fst_snd m nm1) as lem1. *)
-  assert ((fun v => v >= 0) (`m+1)) by (destructAllRefts; smt_infer).
-  set (app1 := ltac:(exists (`m+1); assumption) :Nat ).
-  assert ((fun v => v >= 0) (`n-1)) by (destructAllRefts; smt_infer).
-  set (app2 := ltac:(exists (`n-1); assumption) :Nat ).
-  (* set (mid := ack app1 app2). *)
-  (* #[local] Hint Extern 4 => unfold mid: ackHintDb. *)
-  assert (` (ack app1 app2) >= ` (ack m n) /\ ` (ack m n) >= 0)
+
+  set (nm1 := `` (`n-1)).
+  set (mp1 := ``(`m + 1)).
+  (* assert (` (ack app1 app2) >= ` (ack m n) /\ ` (ack m n) >= 0)
     by (split ;
     [ applys_eq (ack_plus_one_fst_snd m nm1); repeat f_equal; destructAllRefts; apply subset_eq_compat; simpl in *; lia
     | now destruct (ack m n)
     ]) .
-  set (app3 := ltac:(exists (` (ack m n)); assumption):{v:Z| ` (ack app1 app2) >= v /\ v >= 0}).
-
+  set (app3 := ltac:(exists (` (ack m n)); assumption):{v:Z| ` (ack app1 app2) >= v /\ v >= 0}). *)
+  set (ackmn := ack m n).
+  Ltac just_pose m := pose m.
+  apply2 just_pose ack nm1 mp1.
+  reft_pose ack_mon_eq_snd m s.
   eapply ge_ge_ge.
-  * applys_eq (ack_mon_eq_snd m (ack app1 app2)  app3). resolve_eq.
+  * applys_eq H1. resolve_eq.
   * assert (` (ack m n) >= 0) by ( smt_infer).
     set (app4 := ltac:(exists (` (ack m n)); assumption):Nat).
     assert  ( `app4 >= `n+1 /\ `n +1  >= 0).
